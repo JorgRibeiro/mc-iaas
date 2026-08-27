c9cb116fb5febab97e735e70862fea204941e2cb
JorgeCachyOS <yorkis.gamer32@gmail.com>
2026-08-27 20:39:57 -0300

    fix(control-plane): add o gitignore

--- control-plane/frontend/.output/server/_instanceId-Chz7j0aH.mjs
+++
@@ -1,6 +0,0 @@
-import { f as lazyRouteComponent, p as createFileRoute } from "./_libs/@tanstack/react-router+[...].mjs";
-//#region node_modules/.nitro/vite/services/ssr/assets/_instanceId-Chz7j0aH.js
-var $$splitComponentImporter = () => import("./_instanceId-DFmEITid.mjs");
-var Route = createFileRoute("/instances/$instanceId")({ component: lazyRouteComponent($$splitComponentImporter, "component") });
-//#endregion
-export { Route as t };
--- control-plane/frontend/.output/server/_nodeId-C9GPYovG.mjs
+++
@@ -1,6 +0,0 @@
-import { f as lazyRouteComponent, p as createFileRoute } from "./_libs/@tanstack/react-router+[...].mjs";
-//#region node_modules/.nitro/vite/services/ssr/assets/_nodeId-C9GPYovG.js
-var $$splitComponentImporter = () => import("./_nodeId-lq1yorkc.mjs");
-var Route = createFileRoute("/nodes/$nodeId")({ component: lazyRouteComponent($$splitComponentImporter, "component") });
-//#endregion
-export { Route as t };
